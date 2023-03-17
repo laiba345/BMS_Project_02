07d88be209c52a98a2848898dcf8d8ac5a912635
Kang Huang <kangder@KangdeMacBook-Pro.local>
2023-03-17 10:00:03 +0800

    路由配置等

--- vue3_ts_BMS/src/main.ts
+++ vue3_ts_BMS/src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import './assets/css/index.less'
 import 'normalize.css'
+import router from './router'
 
-createApp(App).mount('#app')
+// createApp(App).use(router).mount('#app') 链式写法
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
---
+++ vue3_ts_BMS/src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  //映射关系：path => component
+  routes: [
+    // 路由重定西
+    {
+      path: '/',
+      redirect: '/main'
+    },
+    {
+      path: '/login',
+      component: () => import('../views/login/Login.vue')
+    },
+    {
+      path: '/main',
+      component: () => import('../views/main/Main.vue')
+    }, 
+    {
+        path:'/:pathMatch(.*)', 
+        component:()=> import('../views/not-found/NotFound.vue')
+    }
+  ]
+})
+
+// 路由的导航守卫
+
+export default router
