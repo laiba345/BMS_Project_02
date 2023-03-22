4ec7b565cc58dec1a21cb2918ae2374552b68c95
Kang Huang <kangder@KangdeMBP.lan>
2023-03-22 10:41:49 +0800

    change

--- vue3-ts-cms02/src/service/login/login.ts
+++ vue3-ts-cms02/src/service/login/login.ts
@@ -2,6 +2,7 @@ import hyRequest from '..'
 import type { IAccount } from '@/types'
 
 export function accountLoginRequest(account: IAccount) {
+  // 之前封装好的一些内容
   return hyRequest.post({
     url: '/login',
     data: account
