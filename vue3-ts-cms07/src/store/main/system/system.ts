c54e4a2633ef1207e04b38bf2e0d5eede2ab5872
Null <51855280+laiba345@users.noreply.github.com>
2023-10-18 00:25:19 +0800

    Update system.ts

--- vue3-ts-cms07/src/store/main/system/system.ts
+++ vue3-ts-cms07/src/store/main/system/system.ts
@@ -21,12 +21,15 @@ const useSystemStore = defineStore('system', {
     pageTotalCount: 0
   }),
   actions: {
+    // 在执行 postUsersListAction 操作时，从服务器获取用户列表数据，然后将总数和列表数据分别存储在 store 的状态属性 usersTotalCount 和 usersList 中，
+    // 以便应用中其他组件能够访问和显示这些数据。这是一种通过异步操作来更新 store 状态的常见做法，用于管理应用中的数据。
     async postUsersListAction(queryInfo: any) {
       const usersListResult = await postUsersListData(queryInfo)
       const { totalCount, list } = usersListResult.data
       this.usersTotalCount = totalCount
       this.usersList = list
     },
+    // 其主要作用是删除指定 id 的用户，并在删除成功后重新请求用户列表数据
     async deleteUserByIdAction(id: number) {
       // 1.删除数据操作
       const deleteResult = await deleteUserById(id)
@@ -35,6 +38,7 @@ const useSystemStore = defineStore('system', {
       // 2.重新请求新的数据
       this.postUsersListAction({ offset: 0, size: 10 })
     },
+    // 其主要作用是创建新的用户数据，并在创建成功后重新请求用户列表数据。
     async newUserDataAction(userInfo: any) {
       // 1.创建新的用户
       const newResult = await newUserData(userInfo)
@@ -42,9 +46,8 @@ const useSystemStore = defineStore('system', {
 
       // 2.重新请求新的数据
       this.postUsersListAction({ offset: 0, size: 10 })
-
-
     },
+    //  其主要作用是更新新的用户数据，并在创建成功后重新请求用户列表数据。
     async editUserDataAction(id: number, userInfo: any) {
       // 1.更新用户的数据
       const editResult = await editUserData(id, userInfo)
@@ -55,6 +58,7 @@ const useSystemStore = defineStore('system', {
     },
 
     /** 针对页面的数据: 增删改查 */
+    // 针对具体页面的名字来进行增删改查，目的是为了能够根据不同的页面名字进行抽取操作
     async postPageListAction(pageName: string, queryInfo: any) {
       const pageListResult = await postPageListData(pageName, queryInfo)
       const { totalCount, list } = pageListResult.data
@@ -67,8 +71,7 @@ const useSystemStore = defineStore('system', {
       console.log(deleteResult)
       this.postPageListAction(pageName, { offset: 0, size: 10 })
 
-      // 获取完整的数据
-      // 新建完一些数据，我们想拿到最新的数据展示；需要每次获取最新的数据进行展示
+      // 获取完整的数据； 然后重新请求页面列表数据并获取最新的应用数据以更新应用的状态
       const mainStore = useMainStore()
       mainStore.fetchEntireDataAction()
     },
