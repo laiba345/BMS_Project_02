52ca12430de69f2d6ac2d05a7c46fbb643e29212
Null <51855280+laiba345@users.noreply.github.com>
2023-10-17 23:17:54 +0800

    Update App.vue

--- vue3-ts-cms07/src/App.vue
+++ vue3-ts-cms07/src/App.vue
@@ -2,6 +2,9 @@
   <div class="app">
     <!-- <h2 class="title">当前计数: {{ counter }}</h2>
     <button @click="increment">+1</button> -->
+    // 这是一个 Element Plus 组件，它用于提供全局的配置信息。通过 :locale 属性，你将本地化语言设置为 zhCn，这通常代表中文。
+    // <router-view>：这是一个 Vue Router 的路由视图组件，它用于显示当前路由对应的页面组件。
+    // router-view可以理解为：而是一个占位符或插槽。它是 Vue Router 中的一个核心概念，用于渲染当前路由匹配的组件; 而是用于显示路由匹配组件的地方。通过路由的导航，你可以在应用中动态切换和加载不同的组件，而不必手动管理组件的显示和隐藏。
     <el-config-provider :locale="zhCn">
       <router-view></router-view>
     </el-config-provider>
