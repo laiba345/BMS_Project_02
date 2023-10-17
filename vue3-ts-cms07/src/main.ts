4cfe113181f020e58191de292941ee65eef1b372
Null <51855280+laiba345@users.noreply.github.com>
2023-10-17 23:08:52 +0800

    Update main.ts

--- vue3-ts-cms07/src/main.ts
+++ vue3-ts-cms07/src/main.ts
@@ -2,16 +2,13 @@ import { createApp } from 'vue'
 import 'normalize.css'
 import './assets/css/index.less'
 import App from './App.vue'
-import router from './router'
+import router from './router' 
 import store from './store'
 import icons from './global/register-icons'
 
-const app = createApp(App)
+const app = createApp(App) // 使用 createApp 函数创建了Vue应用的根实例。App 组件通常是应用的根组件，包裹整个应用。
 app.use(icons)
 
-app.use(store)
-app.use(router)
-app.mount('#app')
-
-console.log('Hello Coderwhy')
-console.log('1111122222')
+app.use(store) // 这一行代码使用 Pinia 的 store 配置，将状态管理集成到Vue应用中。
+app.use(router) // 这一行代码使用路由配置，将路由集成到Vue应用中。
+app.mount('#app') // 这一行代码将Vue应用挂载到页面上的一个具有 id 为 'app' 的HTML元素上。这个元素通常是应用的根DOM容器。
