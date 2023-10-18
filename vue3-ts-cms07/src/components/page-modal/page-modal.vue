20ee59cba06b0e9f879e4a9c55683b817bbba13e
Null <51855280+laiba345@users.noreply.github.com>
2023-10-18 15:52:39 +0800

    Update page-modal.vue

--- vue3-ts-cms07/src/components/page-modal/page-modal.vue
+++ vue3-ts-cms07/src/components/page-modal/page-modal.vue
@@ -93,6 +93,13 @@ const editData = ref()
 const systemStore = useSystemStore()
 
 // 2.定义设置dialogVisible方法
+```
+setModalVisible 函数用于控制模态框的显示和隐藏以及初始化模态框的数据。具体作用如下：
+当调用 setModalVisible 时，可以传入一个布尔值 isNew 以及一个可选的参数 itemData。
+如果 isNew 为 true，则表示打开模态框用于新建数据，并会根据 props.modalConfig.formItems 中的配置初始化 formData。
+如果 isNew 为 false 并提供了 itemData，则表示打开模态框用于编辑数据，会将传入的 itemData 的属性值赋给 formData，以便编辑。
+如果 isNew 为 false 但未提供 itemData，则表示打开模态框用于编辑数据，但不会传入初始数据，通常用于编辑一个新数据。
+```
 function setModalVisible(isNew: boolean = true, itemData?: any) {
   dialogVisible.value = true
   isNewRef.value = isNew
@@ -113,6 +120,14 @@ function setModalVisible(isNew: boolean = true, itemData?: any) {
 }
 
 // 3.点击了确定的逻辑
+```
+handleConfirmClick 函数用于处理用户点击模态框中的 "确定" 按钮后的逻辑。具体作用如下：
+
+当用户点击 "确定" 按钮时，会首先隐藏模态框（dialogVisible 设置为 false）。
+然后，根据当前的编辑状态（新建或编辑）以及用户输入的数据（formData）执行不同的操作：
+如果是编辑状态（isNewRef.value 为 false）并且存在编辑数据 (editData.value)，则执行 systemStore.editPageDataAction，用于编辑指定数据。
+否则，执行 systemStore.newPageDataAction，用于创建新的数据。在创建新数据时，将用户输入的数据与 props.otherInfo 合并，然后调用相应的 Vuex action。
+```
 function handleConfirmClick() {
   dialogVisible.value = false
 
