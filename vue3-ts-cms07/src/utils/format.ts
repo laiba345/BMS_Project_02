72a9b9f709e244443c9b186b3dd5cc38c7c875a4
Null <51855280+laiba345@users.noreply.github.com>
2023-10-18 09:38:42 +0800

    Update format.ts

--- vue3-ts-cms07/src/utils/format.ts
+++ vue3-ts-cms07/src/utils/format.ts
@@ -2,7 +2,7 @@ import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 
 dayjs.extend(utc)
-
+// 总的来说，这段代码的作用是提供一个函数 formatUTC，用于将传入的 UTC 时间字符串转换为本地时间，并以指定格式进行格式化。
 export function formatUTC(
   utcString: string,
   format: string = 'YYYY/MM/DD HH:mm:ss'
