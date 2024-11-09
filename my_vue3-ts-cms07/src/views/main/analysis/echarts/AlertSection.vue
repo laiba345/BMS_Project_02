81083f90bc9b9e3b8f92960da7af40a94140f2bb
KK <2846016855@qq.com>
2024-11-09 14:58:29 +0800

    本地相关操作

--- my_vue3-ts-cms07/src/views/main/analysis/echarts/AlertSection.vue
+++ my_vue3-ts-cms07/src/views/main/analysis/echarts/AlertSection.vue
@@ -1,44 +1,44 @@
 <template>
-  <el-timeline style="max-width: 600px">
-    <el-timeline-item
-      v-for="(activity, index) in activities"
-      :key="index"
-      :icon="activity.icon"
-      :type="activity.type"
-      :color="activity.color"
-      :size="activity.size"
-      :hollow="activity.hollow"
-      :timestamp="activity.timestamp"
-    >
-      {{ activity.content }}
-    </el-timeline-item>
-  </el-timeline>
-</template>
-
-<script lang="ts" setup>
-
-const activities = [
-  {
-    content: 'Fusion 1a',
-    timestamp: 'Task to_master execution failed',
-    color: 'rgb(7, 106, 235)'
-  },
-  {
-    content: 'Focsrgbu 10',
-    timestamp: 'Task to_master execution failed',
-    color: 'rgb(7, 106, 235)'
-  },
-  {
-    content: 'Focsrgbu 9',
-    timestamp: 'High Disk Usage exceeds 70%',
-    color: 'rgb(7, 106, 235)'
-  },
-  {
-    content: 'Focsrgbu 8',
-    timestamp: 'Task to_master execution failed',
-    color: 'rgb(7, 106, 235)'
-  }
-]
-</script>
-
-
+    <el-timeline style="max-width: 600px">
+      <el-timeline-item
+        v-for="(activity, index) in activities"
+        :key="index"
+        :color="activity.color"
+        :timestamp="activity.timestamp"
+      >
+        <span :style="{ color: 'rgb(8, 88, 191)' }">
+          {{ activity.content }}
+        </span>
+        <br />
+        <span :style="{ color: 'rgb(127, 134, 143)' }">
+          {{ activity.timestamp }}
+        </span>
+      </el-timeline-item>
+    </el-timeline>
+  </template>
+  
+  <script lang="ts" setup>
+  const activities = [
+    {
+      content: 'Fusion 1a',
+      timestamp: 'Task to_master execution failed',
+      color: 'rgb(7, 106, 235)',
+    },
+    {
+      content: 'Focsrgbu 10',
+      timestamp: 'Task to_master execution failed',
+      color: 'rgb(7, 106, 235)',
+    },
+    {
+      content: 'Focsrgbu 9',
+      timestamp: 'High Disk Usage exceeds 70%',
+      color: 'rgb(7, 106, 235)',
+    },
+    {
+      content: 'Focsrgbu 8',
+      timestamp: 'Task to_master execution failed',
+      color: 'rgb(7, 106, 235)',
+    },
+  ];
+  </script>
+  
